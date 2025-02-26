83f166158c56534dc9a818ca3a4b1e17170c5298
marwanamgad <marwanamgad25@gmail.com>
2025-02-26 05:26:33 +0200

    New Exercises

--- C Programming/Assignment 3/Exercise22/ex22.c
+++ C Programming/Assignment 3/Exercise22/ex22.c
@@ -8,17 +8,4 @@
 
 #include<stdio.h>
 
-int removeDuplicates(int arr_old[], int n_old, int arr_new[], int
-*n_new) {
-	int i;
-	for(int i=0;i<n_old;i++)
-	{
-		if(arr_old[i]==)
-	}
 
-}
-
-int main (void)
-{
-
-}
